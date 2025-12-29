9c3e069e0e7d0b923d4c84520ab751ebf5c5076a
Rusaln-IT <ruslan.r.f@yandex.ru>
2025-12-29 19:19:01 +0500

    Update admin panel category

--- app/Filament/Resources/Categories/Tables/CategoriesTable.php
+++ app/Filament/Resources/Categories/Tables/CategoriesTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Categories\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -14,6 +15,11 @@ class CategoriesTable
     {
         return $table
             ->columns([
+
+                ImageColumn::make('main_image')
+                    ->label('Фото')
+                    ->disk('public')
+                    ->square(),
                 TextColumn::make('name')
                     ->label('Название')
                     ->formatStateUsing(function ($state, $record) {
